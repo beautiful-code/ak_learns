38bc48a4c54f8b75bb9bc58b7c7f94a535da369b
AK <rabhilash@student.nitw.ac.in>
2016-02-16 14:14:04 +0530

    fixed moviesdb

--- js_intro/movies_db/movies.js
+++ js_intro/movies_db/movies.js
@@ -1,16 +1,9 @@
-var moviesDb = { };
-moviesDb.xmen = {
-  title: "Xmen",
-  actors: ["ak", "sj", "pg", "ng"]
+var moviesDb = {
+  movies: [ { title: "avengers", actors: ["ak", "sj", "gp"] }, { title: "ironman", actors: [ "ak", "pg"]} ],
+  movies_acted_in: function(actor_name) {
+    for( var i =0 ; i < this.movies.length; i++) {
+      if(this.movies[i].actors.indexOf(actor_name) != -1)
+        console.log(this.movies[i].title);
+    }
+}
 };
-moviesDb.avengers = {
-  title: "Avengers",
-  actors: ["sj", "pg"]
-}
-
-var movies_acted_in = function(actor_name) {
-  for( movie in moviesDb) {
-    if(moviesDb[movie].actors.indexOf(actor_name) != -1)
-      console.log(moviesDb[movie].title);
-  }
-}
