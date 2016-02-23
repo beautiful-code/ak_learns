9bb96a814cbaa287c3013bfee8389b0abda6a2f4
AK <rabhilash@student.nitw.ac.in>
2016-02-23 17:52:18 +0530

    refactor

--- js_intro/book_shelf/bookshelf.js
+++ js_intro/book_shelf/bookshelf.js
@@ -1,3 +1,4 @@
+"use strict";
 var BookShelf = {
   books: [ { id:1, name: "aaaa", isRead: false}, { id:2, name:'bbbb', isRead: false}, { id:3, name: "ccc", isRead: true}],
   print_unread_books :function() {
@@ -5,20 +6,20 @@ var BookShelf = {
       if(!BookShelf.books[i].isRead)
         console.log(BookShelf.books[i].name);
     }
-},
+  },
   print_read_books : function() {
     for( var i=0; i< BookShelf.books.length;i++) {
       if(BookShelf.books[i].isRead)
         console.log(BookShelf.books[i].name);
     }
-},
+  },
 
- mark_book_read : function(book_id) {
-  for( var i=0; i< BookShelf.books.length;i++) {
+  mark_book_read : function(book_id) {
+    for( var i=0; i< BookShelf.books.length;i++) {
       if(BookShelf.books[i].id === book_id)
         BookShelf.books[i].isRead = true;
     }
-}
+  }
 };
 
 
